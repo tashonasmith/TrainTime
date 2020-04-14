40108ea7e96d2382f2bea6dafcdfa4304cc4c170
Tashona Smith <tashona.smith@gmail.com>
2020-04-14 15:00:51 -0500

    Append form input values to HMTL via firebase childSnapshot

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -4,6 +4,7 @@ var config = {
     databaseURL: "https://train-time-e3c13.firebaseio.com",
     projectId: "train-time-e3c13",
     storageBucket: "train-time-e3c13.appspot.com",
+    messagingSenderId: "251724558937",
 }
 
 firebase.initializeApp(config);
@@ -18,6 +19,7 @@ var minAway = "";
 
 $("#submit").on("click", function(event){
     event.preventDefault();
+    //$(".form-control").val("");
 
     name = $("#name-input").val().trim();
     dest = $("#destination-input").val().trim();
@@ -28,11 +30,77 @@ $("#submit").on("click", function(event){
 
     database.ref().push({
         name: name,
-        destination: dest,
-        firstTrainTime: firstTrain,
+        dest: dest,
+        firstTrain: firstTrain,
         frequency: frequency
       });
 
+
+      /*database.ref().on("child_added", function(childSnapshot){
+        console.log(childSnapshot.val().name);
+        console.log(childSnapshot.val().dest);
+        console.log(childSnapshot.val().firstTrain);
+        console.log(childSnapshot.val().frequency);
+    
+       /*var startDate = Date.parse(childSnapshot.val().date);
+       console.log(startDate);*/
+    
+        /*var row = $("<tr>");
+    
+        var tdname = $("<td>");
+        tdname.text(childSnapshot.val().name);
+    
+        var tddest = $("<td>");
+        tddest.text(childSnapshot.val().dest);
+    
+        var tdfrequency = $("<td>");
+        tdfrequency.text(childSnapshot.val().frequency);
+    
+        var tdnextArrival = $("<td>");
+    
+        var tdminutes = $("<td>");
+        //tdrate.text(childSnapshot.val().frequency);
+    
+        row.append(tdname, tddest, tdfrequency, tdnextArrival, tdminutes);
+        $(".table").append(row);
+    
+    }, function(errorObject) {
+        console.log("The read failed: " + errorObject.code);
+    }); */ 
+
+    $(".form-control").val(""); 
+});
+
+database.ref().on("child_added", function(childSnapshot){
+    console.log(childSnapshot.val().name);
+    console.log(childSnapshot.val().dest);
+    console.log(childSnapshot.val().firstTrain);
+    console.log(childSnapshot.val().frequency);
+
+   /*var startDate = Date.parse(childSnapshot.val().date);
+   console.log(startDate);*/
+
+    var row = $("<tr>");
+
+    var tdname = $("<td>");
+    tdname.text(childSnapshot.val().name);
+
+    var tddest = $("<td>");
+    tddest.text(childSnapshot.val().dest);
+
+    var tdfrequency = $("<td>");
+    tdfrequency.text(childSnapshot.val().frequency);
+
+    var tdnextArrival = $("<td>");
+
+    var tdminutes = $("<td>");
+    //tdrate.text(childSnapshot.val().frequency);
+
+    row.append(tdname, tddest, tdfrequency, tdnextArrival, tdminutes);
+    $(".table").append(row);
+
+}, function(errorObject) {
+    console.log("The read failed: " + errorObject.code);
 });
 
   
